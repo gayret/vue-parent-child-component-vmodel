b6f62a9e2902faf9a8e768ec98400d95c36fba60
Safa Gayret <safa.gayret@tsoft.com.tr>
2022-07-02 15:54:09 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+import Child from './components/Child.vue'
+
+const someThing = ref('Hello')
+</script>
+
+<template>
+  <div class="parent">
+    <h1>{{ someThing }}</h1>
+    <p>This is the parent component.</p>
+  </div>
+  <child v-model="someThing" />
+</template>
+
+<style scoped>
+.parent {
+  padding: 1em;
+  border: 1px solid green;
+}
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+  modelValue: String,
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const value = ref(props.modelValue)
+
+watch(value, (newVal) => {
+  emit('update:modelValue', newVal)
+})
+</script>
+
+<template>
+  <div class="child">
+    <input type="text" v-model="value" />
+    <p>This is the child component.</p>
+  </div>
+</template>
+
+<style scoped>
+.child {
+  padding: 1em;
+  border: 1px solid skyblue;
+  width: 50%;
+}
+</style>
